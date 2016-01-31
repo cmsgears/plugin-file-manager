e901bf208ebab82c1a74f228e93fb49de84848d6
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2016-01-31 20:57:08 +0530

    Applied minor fixes for template system.

--- migrations/db-testdata.sql
+++ migrations/db-testdata.sql
@@ -8,25 +8,25 @@ SELECT @site := `id` FROM cmg_core_site WHERE slug = 'main';
 -- Facebook Config Form
 --
 
-INSERT INTO `cmg_core_form` (`siteId`,`templateId`,`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`description`,`successMessage`,`captcha`,`visibility`,`active`,`userMail`,`adminMail`,`htmlOptions`,`createdAt`,`modifiedAt`) VALUES
-	(@site,NULL,1,1,'Config File','config-file','system','Facebook configuration form.','All configurations saved successfully.',0,10,1,0,0,NULL,'2014-10-11 14:22:54','2014-10-11 14:22:54');
+INSERT INTO `cmg_core_form` (`siteId`,`templateId`,`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`description`,`successMessage`,`captcha`,`visibility`,`active`,`userMail`,`adminMail`,`createdAt`,`modifiedAt`,`htmlOptions`,`data`) VALUES
+	(@site,NULL,1,1,'Config File','config-file','system','Facebook configuration form.','All configurations saved successfully.',0,10,1,0,0,'2014-10-11 14:22:54','2014-10-11 14:22:54',NULL,NULL);
 
 SELECT @form := `id` FROM cmg_core_form WHERE slug = 'config-file';
 
-INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`validators`,`htmlOptions`,`data`,`order`) VALUES 
-	(@form,'image_extensions','Image Extensions',0,0,'required','{\"title\":\"Image Extensions.\",\"placeholder\":\"Image Extensions\"}',NULL,0),
-	(@form,'video_extensions','Video Extensions',0,0,'required','{\"title\":\"Video Extensions.\",\"placeholder\":\"Video Extensions\"}',NULL,0),
-	(@form,'audio_extensions','Audio Extensions',0,0,'required','{\"title\":\"Audio Extensions.\",\"placeholder\":\"Audio Extensions\"}',NULL,0),
-	(@form,'document_extensions','Document Extensions',0,0,'required','{\"title\":\"Document Extensions.\",\"placeholder\":\"Document Extensions\"}',NULL,0),
-	(@form,'compressed_extensions','Compressed Extensions',0,0,'required','{\"title\":\"Compressed Extensions.\",\"placeholder\":\"Compressed Extensions\"}',NULL,0),
-	(@form,'generate_name','Generate Name',40,0,'required','{\"title\":\"Generate Name.\"}',NULL,0),
-	(@form,'pretty_name','Pretty Name',40,0,'required','{\"title\":\"Pretty Name.\"}',NULL,0),
-	(@form,'max_size','Max Size',0,0,'required','{\"title\":\"Max Size.\",\"placeholder\":\"Max Size\"}',NULL,0),
-	(@form,'generate_thumb','Generate Thumb',40,0,'required','{\"title\":\"Generate Thumb.\"}',NULL,0),
-	(@form,'thumb_width','Thumb Width',0,0,'required','{\"title\":\"Thumb Width.\",\"placeholder\":\"Thumb Width\"}',NULL,0),
-	(@form,'thumb_height','Thumb Height',0,0,'required','{\"title\":\"Thumb Height.\",\"placeholder\":\"Thumb Height\"}',NULL,0),
-	(@form,'uploads_directory','Uploads Directory',0,0,NULL,'{\"title\":\"Uploads Directory.\",\"placeholder\":\"Uploads Directory\"}',NULL,0),
-	(@form,'uploads_url','Uploads URL',0,0,'required','{\"title\":\"Uploads URL.\",\"placeholder\":\"Uploads URL\"}',NULL,0);
+INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`validators`,`order`,`htmlOptions`,`data`) VALUES 
+	(@form,'image_extensions','Image Extensions',0,0,'required',0,'{\"title\":\"Image Extensions.\",\"placeholder\":\"Image Extensions\"}',NULL),
+	(@form,'video_extensions','Video Extensions',0,0,'required',0,'{\"title\":\"Video Extensions.\",\"placeholder\":\"Video Extensions\"}',NULL),
+	(@form,'audio_extensions','Audio Extensions',0,0,'required',0,'{\"title\":\"Audio Extensions.\",\"placeholder\":\"Audio Extensions\"}',NULL),
+	(@form,'document_extensions','Document Extensions',0,0,'required',0,'{\"title\":\"Document Extensions.\",\"placeholder\":\"Document Extensions\"}',NULL),
+	(@form,'compressed_extensions','Compressed Extensions',0,0,'required',0,'{\"title\":\"Compressed Extensions.\",\"placeholder\":\"Compressed Extensions\"}',NULL),
+	(@form,'generate_name','Generate Name',40,0,'required',0,'{\"title\":\"Generate Name.\"}',NULL),
+	(@form,'pretty_name','Pretty Name',40,0,'required',0,'{\"title\":\"Pretty Name.\"}',NULL),
+	(@form,'max_size','Max Size',0,0,'required',0,'{\"title\":\"Max Size.\",\"placeholder\":\"Max Size\"}',NULL),
+	(@form,'generate_thumb','Generate Thumb',40,0,'required',0,'{\"title\":\"Generate Thumb.\"}',NULL),
+	(@form,'thumb_width','Thumb Width',0,0,'required',0,'{\"title\":\"Thumb Width.\",\"placeholder\":\"Thumb Width\"}',NULL),
+	(@form,'thumb_height','Thumb Height',0,0,'required',0,'{\"title\":\"Thumb Height.\",\"placeholder\":\"Thumb Height\"}',NULL),
+	(@form,'uploads_directory','Uploads Directory',0,0,NULL,0,'{\"title\":\"Uploads Directory.\",\"placeholder\":\"Uploads Directory\"}',NULL),
+	(@form,'uploads_url','Uploads URL',0,0,'required',0,'{\"title\":\"Uploads URL.\",\"placeholder\":\"Uploads URL\"}',NULL);
 
 --
 -- Dumping data for table `cmg_core_model_attribute`
