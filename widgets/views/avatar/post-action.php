98f4e6bce22200adea2ae644c05701e550c62bc3
Lokendra Chouhan <lokendra.chouhan@vulpinecode.com>
2017-06-10 16:32:54 +0530

    include cmt-form and cmt-click class

--- widgets/views/avatar/post-action.php
+++ widgets/views/avatar/post-action.php
@@ -15,16 +15,16 @@ if( $postAction && isset( $postActionUrl ) ) {
 		$paClass = 'post-action-v';
 	}
 ?>
-	<form></form>
+	
 	<div class="<?= $paClass ?>">
-		<form cmt-app="<?= $cmtApp ?>" cmt-controller="<?= $cmtController ?>" cmt-action="<?= $cmtAction ?>" action="<?= $postActionUrl ?>" method='post'>
+		<form class="cmt-form" cmt-app="<?= $cmtApp ?>" cmt-controller="<?= $cmtController ?>" cmt-action="<?= $cmtAction ?>" action="<?= $postActionUrl ?>" method='post'>
 			<div class="max-area-cover spinner">
 				<div class="valign-center cmti cmti-2x cmti-spinner-1 spin"></div>
 			</div>
 			<?= $attributesHtml ?>
 			<?= $infoHtml ?>
 			<div class="frm-actions align align-center">
-				<input class="element-medium" type="submit" value="Save" />
+				<input class="element-medium cmt-click" type="submit" value="Save" />
 			</div>
 		</form>
 	</div>
