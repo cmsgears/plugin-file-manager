3153d453ac4bfdeecd4089363a7db36ac2077586
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-04-14 11:36:15 +0530

    Applied quick fixes.

--- migrations/m160622_061028_file_manager.php
+++ migrations/m160622_061028_file_manager.php
@@ -79,26 +79,26 @@ class m160622_061028_file_manager extends Migration {
 
 		$config = Form::findBySlugType( 'config-file', CoreGlobal::TYPE_SYSTEM );
 
-		$columns = [ 'formId', 'name', 'label', 'type', 'compress', 'validators', 'order', 'icon', 'htmlOptions' ];
+		$columns = [ 'formId', 'name', 'label', 'type', 'compress', 'meta', 'active', 'validators', 'order', 'icon', 'htmlOptions' ];
 
 		$fields = [
-			[ $config->id, 'image_extensions', 'Image Extensions', FormField::TYPE_TEXT, false, 'required', 0, NULL, '{"title":"Image Extensions","placeholder":"Image Extensions"}' ],
-			[ $config->id, 'video_extensions', 'Video Extensions', FormField::TYPE_TEXT, false, 'required', 0, NULL, '{"title":"Video Extensions","placeholder":"Video Extensions"}' ],
-			[ $config->id, 'audio_extensions', 'Audio Extensions', FormField::TYPE_TEXT, false, 'required', 0, NULL, '{"title":"Audio Extensions","placeholder":"Audio Extensions"}' ],
-			[ $config->id, 'document_extensions', 'Document Extensions', FormField::TYPE_TEXT, false, 'required', 0, NULL, '{"title":"Document Extensions","placeholder":"Document Extensions"}' ],
-			[ $config->id, 'compressed_extensions', 'Compressed Extensions', FormField::TYPE_TEXT, false, 'required', 0, NULL, '{"title":"Compressed Extensions","placeholder":"Compressed Extensions"}' ],
-			[ $config->id, 'generate_name', 'Generate Name', FormField::TYPE_TOGGLE, false, 'required', 0, NULL, '{"title":"Generate Name"}' ],
-			[ $config->id, 'pretty_name', 'Pretty Name', FormField::TYPE_TOGGLE, false, 'required', 0, NULL, '{"title":"Pretty Name"}' ],
-			[ $config->id, 'max_size', 'Max Size', FormField::TYPE_TEXT, false, 'required', 0, NULL, '{"title":"Max Size","placeholder":"Max Size"}' ],
-			[ $config->id, 'max_resolution', 'Max Resolution', FormField::TYPE_TEXT, false, 'required', 0, NULL, '{"title":"Max Resolution - limits pixel count","placeholder":"Max Resolution"}' ],
-			[ $config->id, 'generate_medium', 'Generate Medium', FormField::TYPE_TOGGLE, false, 'required', 0, NULL, '{"title":"Generate Medium Image"}' ],
-			[ $config->id, 'generate_thumb', 'Generate Thumb', FormField::TYPE_TOGGLE, false, 'required', 0, NULL, '{"title":"Generate Thumb Image"}' ],
-			[ $config->id, 'medium_width', 'Medium Width', FormField::TYPE_TEXT, false, 'required', 0, NULL, '{"title":"Medium Width","placeholder":"Medium Width"}' ],
-			[ $config->id, 'medium_height', 'Medium Height', FormField::TYPE_TEXT, false, 'required', 0, NULL, '{"title":"Medium Height","placeholder":"Medium Height"}' ],
-			[ $config->id, 'thumb_width', 'Thumb Width', FormField::TYPE_TEXT, false, 'required', 0, NULL, '{"title":"Thumb Width","placeholder":"Thumb Width"}' ],
-			[ $config->id, 'thumb_height', 'Thumb Height', FormField::TYPE_TEXT, false, 'required', 0, NULL, '{"title":"Thumb Height","placeholder":"Thumb Height"}' ],
-			[ $config->id, 'uploads_directory', 'Uploads Directory', FormField::TYPE_TEXT, false, NULL, 0, NULL, '{"title":"Uploads Directory","placeholder":"Uploads Directory"}' ],
-			[ $config->id, 'uploads_url', 'Uploads URL', FormField::TYPE_TEXT, false, 'required', 0, NULL, '{"title":"Uploads URL","placeholder":"Uploads URL"}' ]
+			[ $config->id, 'image_extensions', 'Image Extensions', FormField::TYPE_TEXT, false, true, true, 'required', 0, NULL, '{"title":"Image Extensions","placeholder":"Image Extensions"}' ],
+			[ $config->id, 'video_extensions', 'Video Extensions', FormField::TYPE_TEXT, false, true, true, 'required', 0, NULL, '{"title":"Video Extensions","placeholder":"Video Extensions"}' ],
+			[ $config->id, 'audio_extensions', 'Audio Extensions', FormField::TYPE_TEXT, false, true, true, 'required', 0, NULL, '{"title":"Audio Extensions","placeholder":"Audio Extensions"}' ],
+			[ $config->id, 'document_extensions', 'Document Extensions', FormField::TYPE_TEXT, false, true, true, 'required', 0, NULL, '{"title":"Document Extensions","placeholder":"Document Extensions"}' ],
+			[ $config->id, 'compressed_extensions', 'Compressed Extensions', FormField::TYPE_TEXT, false, true, true, 'required', 0, NULL, '{"title":"Compressed Extensions","placeholder":"Compressed Extensions"}' ],
+			[ $config->id, 'generate_name', 'Generate Name', FormField::TYPE_TOGGLE, false, true, true, 'required', 0, NULL, '{"title":"Generate Name"}' ],
+			[ $config->id, 'pretty_name', 'Pretty Name', FormField::TYPE_TOGGLE, false, true, true, 'required', 0, NULL, '{"title":"Pretty Name"}' ],
+			[ $config->id, 'max_size', 'Max Size', FormField::TYPE_TEXT, false, true, true, 'required', 0, NULL, '{"title":"Max Size","placeholder":"Max Size"}' ],
+			[ $config->id, 'max_resolution', 'Max Resolution', FormField::TYPE_TEXT, false, true, true, 'required', 0, NULL, '{"title":"Max Resolution - limits pixel count","placeholder":"Max Resolution"}' ],
+			[ $config->id, 'generate_medium', 'Generate Medium', FormField::TYPE_TOGGLE, false, true, true, 'required', 0, NULL, '{"title":"Generate Medium Image"}' ],
+			[ $config->id, 'generate_thumb', 'Generate Thumb', FormField::TYPE_TOGGLE, false, true, true, 'required', 0, NULL, '{"title":"Generate Thumb Image"}' ],
+			[ $config->id, 'medium_width', 'Medium Width', FormField::TYPE_TEXT, false, true, true, 'required', 0, NULL, '{"title":"Medium Width","placeholder":"Medium Width"}' ],
+			[ $config->id, 'medium_height', 'Medium Height', FormField::TYPE_TEXT, false, true, true, 'required', 0, NULL, '{"title":"Medium Height","placeholder":"Medium Height"}' ],
+			[ $config->id, 'thumb_width', 'Thumb Width', FormField::TYPE_TEXT, false, true, true, 'required', 0, NULL, '{"title":"Thumb Width","placeholder":"Thumb Width"}' ],
+			[ $config->id, 'thumb_height', 'Thumb Height', FormField::TYPE_TEXT, false, true, true, 'required', 0, NULL, '{"title":"Thumb Height","placeholder":"Thumb Height"}' ],
+			[ $config->id, 'uploads_directory', 'Uploads Directory', FormField::TYPE_TEXT, false, true, true, NULL, 0, NULL, '{"title":"Uploads Directory","placeholder":"Uploads Directory"}' ],
+			[ $config->id, 'uploads_url', 'Uploads URL', FormField::TYPE_TEXT, false, true, true, 'required', 0, NULL, '{"title":"Uploads URL","placeholder":"Uploads URL"}' ]
 		];
 
 		$this->batchInsert( $this->prefix . 'core_form_field', $columns, $fields );
