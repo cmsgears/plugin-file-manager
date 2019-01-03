d1aa888ca7b9c991501ee7c2fdfef8bd8d8ab2ec
Lokendra Chouhan <lokendra.chouhan@vulpinecode.com>
2019-01-03 11:20:28 +0530

    updated code

--- widgets/views/shared/info.php
+++ widgets/views/shared/info.php
@@ -7,7 +7,7 @@ $infoFields		= $widget->infoFields;
 ?>
 <div class="file-info">
 <?php if( isset( $model ) ) { ?>
-	<input type="hidden" name="<?= $modelClass ?>[id]" value="<?= $model->id ?>" />
+	<input type="hidden" name="<?= $modelClass ?>[id]" class="id" value="<?= $model->id ?>" />
 	<input type="hidden" name="<?= $modelClass ?>[name]" class="name" value="<?= $model->name ?>" />
 	<input type="hidden" name="<?= $modelClass ?>[extension]" class="extension" value="<?= $model->extension ?>" />
 	<input type="hidden" name="<?= $modelClass ?>[directory]" value="<?= $widget->directory ?>" />
--- widgets/views/video/info.php
+++ widgets/views/video/info.php
@@ -5,7 +5,7 @@ $modelClass	= $widget->modelClass;
 if( isset( $model ) ) {
 ?>
 	<div class="file-info">
-		<input type="hidden" name="<?= $modelClass ?>[id]" value="<?= $model->id ?>" />
+		<input type="hidden" name="<?= $modelClass ?>[id]" class="id" value="<?= $model->id ?>" />
 		<input type="hidden" name="<?= $modelClass ?>[name]" class="name" value="<?= $model->name ?>" />
 		<input type="hidden" name="<?= $modelClass ?>[extension]" class="extension" value="<?= $model->extension ?>" />
 		<input type="hidden" name="<?= $modelClass ?>[directory]" value="<?= $widget->directory ?>" />
